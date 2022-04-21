27e2856b0f309e87a46f21fe2d0f56e8dec151c3
Leonardorossato <leonardorossato21@gmail.com>
2022-04-21 10:10:27 -0300

    import da conexaõ com o mongoDB e funcioando

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const PORT = process.env.PORT
+const mongoConnection = require('./connection/mongoConnection')
 
 app.use(express.json())
+app.mongoConnection = mongoConnection
 
 app.listen(PORT, ()=> {
     console.log(`Server is running at  port : ${PORT}`)
