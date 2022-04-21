0ab8f5e9cba00f5a480badd70ed83e0470f20a02
Leonardorossato <leonardorossato21@gmail.com>
2022-04-21 10:32:27 -0300

    import da roda de post e funcionando

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const app = express()
 require('dotenv').config()
 const PORT = process.env.PORT
 const mongoConnection = require('./connection/mongoConnection')
+const postRouter = require('./routes/postRouter')
 
 app.use(express.json())
 app.mongoConnection = mongoConnection
+
+app.use('/api/posts', postRouter)
 
 app.listen(PORT, ()=> {
     console.log(`Server is running at  port : ${PORT}`)
